a4e74a8ca5e9c9d221c467e6b652a1468ca7b4a6
Larissa Pavan <95706520+larissacpavan@users.noreply.github.com>
2025-03-26 21:25:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import random
 
 def guess(x):
-    random_number = random.randint(1,x)
+    random_number = random.randint(1, x)
     guess = 0
     while guess != random_number:
         guess = int(input(f'Guess a number between 1 and {x}: '))
-        if guess < random_number: 
-          print ('Sorry, guess again. Too low.')
+        if guess < random_number:
+            print('Sorry, guess again. Too low.')
         elif guess > random_number:
-          print ('Sorry, guess again. Too high.')       
-                     
-guess (10)
+            print('Sorry, guess again. Too high.')
+    
+    print(f'Yeaaah, congrats!!! You guessed the number {random_number} correctly!!!')
 
-print ('Yeaaah, congrats!!! You guessed the number. {random.number} correctly!!!' )
+# Start the game
+guess(10)
